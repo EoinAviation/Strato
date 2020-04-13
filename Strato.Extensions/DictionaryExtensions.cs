c50c8839e27b490a7fa9a44fabeb5ad7b1ce659b
Eoin Motherway <eoinmoth@yahoo.ie>
2020-04-13 14:37:00 +1000

    Added extensions project

---
+++ Strato.Extensions/DictionaryExtensions.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DictionaryExtensions.cs" company="Strato Systems Pty. Ltd.">
+//   Copyright (c) Strato Systems Pty. Ltd. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Strato.Extensions
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     The <see cref="Dictionary{TKey,TValue}"/> extensions.
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        ///     Adds a new <see cref="KeyValuePair{TKey,TValue}"/> to the <paramref name="dictionary"/>, or updates an
+        ///     existing <see cref="KeyValuePair{TKey,TValue}"/> with the <paramref name="value"/> where the keys
+        ///     match.
+        /// </summary>
+        /// <typeparam name="TKey">
+        ///     The type of the key.
+        /// </typeparam>
+        /// <typeparam name="TValue">
+        ///     The type of the value.
+        /// </typeparam>
+        /// <param name="dictionary">
+        ///     The <see cref="Dictionary{TKey,TValue}"/>.
+        /// </param>
+        /// <param name="key">
+        ///     The key.
+        /// </param>
+        /// <param name="value">
+        ///     The value.
+        /// </param>
+        public static void AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key] = value;
+            }
+            else
+            {
+                dictionary.Add(key, value);
+            }
+        }
+    }
+}
---
+++ Strato.Extensions/TaskExtensions.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TaskExtensions.cs" company="Strato Systems Pty. Ltd.">
+//   Copyright (c) Strato Systems Pty. Ltd. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Strato.Extensions
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///     The <see cref="Task"/> extension methods.
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        ///     Fires and forgets an async <see cref="Task"/>.
+        /// </summary>
+        /// <param name="task">
+        ///     The <see cref="Task"/>.
+        /// </param>
+        /// <param name="onException">
+        ///     The <see cref="Action{T}"/> to handle <see cref="Exception"/>s.
+        /// </param>
+#pragma warning disable RECS0165 // Asynchronous methods should return a Task instead of void
+        public static async void FireAndForgetSafeAsync(this Task task, Action<Exception> onException = null)
+#pragma warning restore RECS0165 // Asynchronous methods should return a Task instead of void
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception exception)
+            {
+                onException?.Invoke(exception);
+            }
+        }
+    }
+}
